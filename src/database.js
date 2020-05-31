29b57d2ced8b139cd68d2c7ad5b310d1d4e79712
Antonio <antonio.salcedo@ultertechnologies.com>
2020-05-31 09:32:14 -0500

    heroku app

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const URI = 'mongodb://localhost/crud'
+const URI = 'mongodb://heroku_s68mzq5z:sefb6gl1n7h9pt3ijqbeve08p9@ds035014.mlab.com:35014/heroku_s68mzq5z'
 mongoose.connect(URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err))
